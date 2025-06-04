319e61dcea4d37cd93e6f284959d130491336a9c
marcelo aires das neves <mneves230@gmail.com>
2025-06-04 09:09:25 -0300

    Testing class JAVA

--- src/br/com/br/heranca/HelloMarcelo.java
+++ src/br/com/br/heranca/HelloMarcelo.java
@@ -2,6 +2,6 @@ package br.com.br.heranca;
 
 public class HelloMarcelo {
     public static void main(String[] args) {
-        System.out.println("Hello Marcelo");
+        System.out.println("Hello Marcelo Aires das Neves");
     }
 }
