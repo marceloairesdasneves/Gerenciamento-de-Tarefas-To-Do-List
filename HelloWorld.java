ac5803b5d3e695006ed1554248b4dada4bd1aa83
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-06-04 09:02:28 -0300

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello World! This is my first Java program.");
-
+        System.out.println("Hello, World!");
     }
 }
