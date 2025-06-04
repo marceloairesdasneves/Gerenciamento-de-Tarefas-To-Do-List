72c5974ee79baa4e16f1ed90641da04a40e13b80
marcelo aires das neves <mneves230@gmail.com>
2025-06-04 09:17:04 -0300

    Hello meu first pass git

--- src/br/com/br/heranca/HelloMarcelo.java
+++ src/br/com/br/heranca/HelloMarcelo.java
@@ -2,6 +2,6 @@ package br.com.br.heranca;
 
 public class HelloMarcelo {
     public static void main(String[] args) {
-        System.out.println("Hello Marcelo Aires");
+        System.out.println("Hello meu first pass git");
     }
 }
