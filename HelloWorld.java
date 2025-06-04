36cacdda41baa917ec3aac9f2d4de7aa6d716bf4
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-06-04 00:35:59 +0000

    testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,10 @@ public class HelloWorld{
         System.out.println("It prints messages to the console.");
         System.out.println("Java is a versatile programming language.");
         System.out.println("Let's learn more about Java together!");
+        System.out.println("Happy coding!");
+        System.out.println("End of the program.");
+        System.out.println("Goodbye!");
+        System.out.println("Thank you for running this program.");
+        System.out.println("Feel free to modify and experiment with the code.");
     }
 }
