bbe59444f34132397db29bddaf5cae2bed8248fd
Marcelo Neves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-06-04 00:38:21 +0000

    The first testing integration to-do-list2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("This is a simple Java program.");
-        System.out.println("It prints messages to the console.");
-        System.out.println("Java is a versatile programming language.");
-        System.out.println("Let's learn more about Java together!");
-        System.out.println("Happy coding!");
-        System.out.println("End of the program.");
-        System.out.println("Goodbye!");
-        System.out.println("Thank you for running this program.");
-        System.out.println("Feel free to modify and experiment with the code.");
+        System.out.println("Hello World! This is my first Java program.");
+
     }
 }
