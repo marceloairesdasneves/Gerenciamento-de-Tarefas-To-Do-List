a0263c357213cae6ab0866d279ee799cc52731a7
marcelo aires das neves <mneves230@gmail.com>
2025-06-04 09:10:00 -0300

    Testing Marcelo alter.

--- src/br/com/br/heranca/HelloMarcelo.java
+++ src/br/com/br/heranca/HelloMarcelo.java
@@ -2,6 +2,6 @@ package br.com.br.heranca;
 
 public class HelloMarcelo {
     public static void main(String[] args) {
-        System.out.println("Hello Marcelo Aires das Neves");
+        System.out.println("Hello Marcelo Aires");
     }
 }
